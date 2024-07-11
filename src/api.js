fa4ab88002ae3cf24fd676386520debbbde7a5b5
Seyma Bayezit <163021404+seymabyzt@users.noreply.github.com>
2024-07-11 22:38:09 +0300

    url

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 export async function getData() {
-    const res = await fetch('http://localhost:8000/products')
+    const res = await fetch('https://e-commerce-eta-gules.vercel.app:8000/products')
     if (!res.ok) {
       throw new Error('Failed to fetch data')
     }
@@ -8,7 +8,7 @@ export async function getData() {
   }
 
   export async function getProduct(params) {
-    const res = await fetch(`http://localhost:8000/products/${params.id}`)
+    const res = await fetch(`https://e-commerce-eta-gules.vercel.app:8000/products/${params.id}`)
     if (!res.ok) {
       throw new Error('Failed to fetch data')
     }
@@ -17,7 +17,7 @@ export async function getData() {
   }
 
   export async function getReviews(productId) {
-    const res = await fetch(`http://localhost:8000/reviews/?productId=${productId}`)
+    const res = await fetch(`https://e-commerce-eta-gules.vercel.app:8000/reviews/?productId=${productId}`)
     if (!res.ok) {
       throw new Error('Failed to fetch data')
     }
